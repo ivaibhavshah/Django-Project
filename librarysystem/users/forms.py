0c2ff680e13cc409b8e3c8e4e8db5ef72a4a4099
Vaibhav Abhaybhai Shah <vaibhav.anjar@gmail.com>
2022-07-12 18:20:53 +0530

    all set!!!

--- librarysystem/users/forms.py
+++ librarysystem/users/forms.py
@@ -3,13 +3,16 @@ from django import forms
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
+    
+    username = forms.CharField(label = "Username",widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(label = "Email",widget=forms.TextInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(label = "First name",widget=forms.TextInput(attrs={'class':'form-control'}))
     last_name = forms.CharField(label = "Last name",widget=forms.TextInput(attrs={'class':'form-control'}))
-
+    password1 = forms.CharField(label = "Set Password",widget=forms.TextInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label = "Confirm Password",widget=forms.TextInput(attrs={'class':'form-control'}))
     class Meta:
         model = User
-        fields = ("username", "email", )
+        fields = ("username", "email","first_name","last_name" ,"password1","password2")
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
         user.first_name = self.cleaned_data["first_name"]
